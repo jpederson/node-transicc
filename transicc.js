39775396c2a877c6a3a240e6ccb2bace19f86577
jpederson <james@jpederson.com>
2014-06-07 11:51:31 -0500

    Adding a switch function for the shorthand profile names, so they'll work without the symlinks (since npm destroys them when it installs).

--- transicc.js
+++ transicc.js
@@ -2,6 +2,41 @@
 
 var spawn = require( "child_process" ).spawn;
 
+
+// new switch in 1.1.2 to replace symlinks for shortcut profiles
+var profile_path = function( profile_name ){
+	switch ( profile_name ) {
+
+		// use Adobe's CMYK profile
+		case "cmyk":
+			profile_name = "cmyk-adobe-us-web-coated-swop";
+		break;
+
+		// use Apple's lab profile
+		case "lab":
+			profile_name = "lab-apple";
+		break;
+
+		// use AdobeRGB for RGB by default
+		case "rgb":
+			profile_name = "rgb-adobe-1998";
+		break;
+
+		// use Apple's sRGB profile
+		case "srgb":
+			profile_name = "srgb-apple";
+		break;
+
+		// use XYZ with a luminant of d65 by default
+		case "xyz":
+			profile_name = "xyz-d65";
+		break;
+	}
+
+	return "profile/"+profile_name+".icc";
+}
+
+
 // generic function to pass profiles and parameters to the
 // LittleCMS transicc command line utility.
 module.exports = function( input_profile, output_profile, args, callback ) {
@@ -10,10 +45,10 @@ module.exports = function( input_profile, output_profile, args, callback ) {
 	var cmd_arg = [];
 
 	// add the input profile
-	cmd_arg.push( ( input_profile.match(".icc") ? "../../" . input_profile : "profile/"+input_profile+".icc" ) );
+	cmd_arg.push( profile_path( input_profile ) );
 
 	// add the output profile
-	cmd_arg.push( ( output_profile.match(".icc") ? "../../" . output_profile : "profile/"+output_profile+".icc" ) );
+	cmd_arg.push( profile_path( output_profile ) );
 
 	// loop through the arguments and add them to the command args
 	for ( var i = 0; i < args.length; i++ ) {
@@ -34,6 +69,10 @@ module.exports = function( input_profile, output_profile, args, callback ) {
 		response = new String( data ).split( " " );
 
 	});
+
+
+	// output errors to the console - only for debugging.
+	// cmd.stderr.on('data', function (data) { console.log('stderr: ' + data); });
 
 
 	// once the command is finished
